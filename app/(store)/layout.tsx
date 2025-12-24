00f8e9a53dd8c9597f349d9cba6f51eee54f3b74
RohitBavkar <rohitbavkar95@gmail.com>
2025-12-25 00:55:16 +0530

    Header and loader for main page

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,13 +1,18 @@
+import { Header } from "@/components/Header/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
-        <main>{children}</main>
-        <Toaster position="bottom-center" />
+        <ChatStoreProvider>
+          <Header />
+          <main>{children}</main>
+          <Toaster position="bottom-center" />
+        </ChatStoreProvider>
       </CartStoreProvider>
     </ClerkProvider>
   );
